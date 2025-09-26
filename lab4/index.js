dff67a02b476ec679855eed17b627cb58563d802
Fidan Zeynalli <fidan.zeynalli@georgebrown.ca>
2025-09-26 13:43:49 -0400

    lab 4

--- lab4/index.js
+++ lab4/index.js
@@ -8,67 +8,29 @@ app.use("/test", express.static("./public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// http://localhost:8089/
-app.get("/", (req, res) => {
-    res.status(201).send("<h1>Hello Express JS</h1>")
-})
-
-// http://localhost:8089/home
-app.get("/home", (req, res) => {
-    res.send("Home Page")
+// GET /hello
+app.get("/hello", (req, res) => {
+    res.type("text/plain").send("Hello Express JS");
 });
 
-// http://localhost:8089/person
-app.post("/person", (req, res) => {
-    const p = {
-        pid: 1,
-        pnm: "Pritesh Patel"
-    }
-
-    res.send(JSON.stringify(p))
-})
-
-// http://localhost:8089/person
-app.get("/person", (req, res) => {
-    const p = {
-        pid: 1,
-        pnm: "Pritesh Patel",
-        city: "Toronto"
-    }
-
-    res.json(p)
-})
-
-// Path Parameter
-// http://localhost:8089/student/pritesh/patel
-app.get("/student/:fname/:lname", (req, res) => {
-    const {fname, lname} = req.params
-    res.send(`Welcome ${fname} ${lname}`)
-})
-
-// Query Parameter
-// http://localhost:8089/user?fnm=pritesh&lnm=patel
+// GET /user?firstname=&lastname=
 app.get("/user", (req, res) => {
-    const {fnm, lnm} = req.query;
+    const firstname = req.query.firstname || "Fidan";
+    const lastname = req.query.lastname || "Zeynalli";
+    res.json({ firstname, lastname });
+});
 
-    if (!fnm || !lnm) {
-        return res.status(400).send("Please provide first name and last name as query parameters.")
-    }
+// POST /user/:firstname/:lastname
+app.post("/user/:firstname/:lastname", (req, res) => {
+    const { firstname, lastname } = req.params;
+    res.json({ firstname, lastname });
+});
 
-    res.send(`${fnm} ${lnm}`)
-})
-
-// Get data asa a body parameter
-// http://localhost:8089/faculty
-app.post("/faculty", (req, res) => {
-    let data = req.body; // get body data
-    console.log("Received Data: ", data);
-
-    res.json({
-        message: "Faculty data received successfully",
-        received: data
-    })
-})
+// POST /users  (expects an array of { firstname, lastname })
+app.post("/users", (req, res) => {
+    const users = Array.isArray(req.body) ? req.body : [];
+    res.json(users);
+});
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server running at http://localhost:${SERVER_PORT}/`);
